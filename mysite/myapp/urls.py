67fa41df0f53f665b704a9421efe970ac1b20cfa
Kate <kjrinkit@gmail.com>
2024-12-30 19:26:49 +0300

    Functional views have been rewritten as class-based views

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from myapp.views import add_item, update_item, delete_item, ProductListView, ProductDetailView, ProductDeleteView
+from myapp.views import(
+    ProductListView, 
+    ProductDetailView, 
+    ProductDeleteView, 
+    ProductCreateView,
+    ProductUpdateView,
+)
 
 app_name = "myapp"
 
@@ -10,8 +16,10 @@ urlpatterns = [
 #   path("<int:my_id>/", indexItem, name="detail"),
     path("<int:pk>/", ProductDetailView.as_view(), name="detail"),
     
-    path("additem/", add_item, name="add_item"),
-    path("updateitem/<int:my_id>/", update_item, name="update_item"),
+#   path("additem/", add_item, name="add_item"),
+    path("additem/", ProductCreateView.as_view(), name="add_item"),
+#   path("updateitem/<int:my_id>/", update_item, name="update_item"),
+    path('updateitem/<int:pk>/', ProductUpdateView.as_view(), name='update_item'),
 #   path("deleteitem/<int:my_id>/", delete_item, name="delete_item"),
     path("deleteitem/<int:pk>/", ProductDeleteView.as_view(), name="delete_item"),
 
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import DeleteView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.core.paginator import Paginator
 
@@ -18,6 +19,7 @@ class ProductListView(ListView):
     model = Product
     template_name = "myapp/index.html"
     context_object_name = "items"
+    paginate_by = 10
 
 
 #def indexItem(request, my_id):
@@ -32,39 +34,57 @@ class ProductDetailView(DetailView):
     template_name = "myapp/detail.html"
     context_object_name = "item"
 
-@login_required
-def add_item(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        price = request.POST.get("price")
-        description = request.POST.get("description")
-        image = request.FILES['upload']
-        seller = request.user
-        item = Product(name=name, price=price, description=description, image=image, seller=seller)
-        item.save()
-    return render(request, "myapp/additem.html" )
+#@login_required
+#def add_item(request):
+#    if request.method == "POST":
+#        name = request.POST.get("name")
+#        price = request.POST.get("price")
+#        description = request.POST.get("description")
+#        image = request.FILES['upload']
+#        seller = request.user
+#        item = Product(name=name, price=price, description=description, image=image, seller=seller)
+#        item.save()
+#    return render(request, "myapp/additem.html" )
 
-def update_item(request, my_id):
-    item = Product.objects.get(id=my_id)
-    if request.method == "POST":
-        item.name = request.POST.get("name")
-        item.price = request.POST.get("price")
-        item.description = request.POST.get("description")
-        item.image = request.FILES.get('upload', item.image)
-        item.save()
-        return redirect("/myapp/")
-    context = {'item': item}
-    return render(request, "myapp/updateitem.html", context)
 
-def delete_item(request, my_id):
-    item = Product.objects.get(id=my_id)
-    if request.method == "POST":
-        item.delete()
-        return redirect("/myapp/")
-    context = {'item': item}
-    return render(request, "myapp/deleteitem.html", context)
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    template_name = "myapp/additem.html"
+    fields = ['seller', 'name', 'price', 'description', 'image']
+    success_url = reverse_lazy('myapp:index')
+
+
+
+#def update_item(request, my_id):
+#    item = Product.objects.get(id=my_id)
+#    if request.method == "POST":
+#        item.name = request.POST.get("name")
+#        item.price = request.POST.get("price")
+#        item.description = request.POST.get("description")
+#        item.image = request.FILES.get('upload', item.image)
+#        item.save()
+#        return redirect("/myapp/")
+#    context = {'item': item}
+#    return render(request, "myapp/updateitem.html", context)
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'myapp/updateitem.html'
+    context_object_name = 'item'
+    fields = ['name', 'price', 'description', 'image']
+    success_url = reverse_lazy('myapp:index')
+
+#def delete_item(request, my_id):
+#    item = Product.objects.get(id=my_id)
+#    if request.method == "POST":
+#        item.delete()
+#        return redirect("/myapp/")
+#    context = {'item': item}
+#    return render(request, "myapp/deleteitem.html", context)
 
 class ProductDeleteView(DeleteView):
     model = Product
+    template_name = 'myapp/product_confirm_delete.html'
     success_url = reverse_lazy("myapp:index")
 
